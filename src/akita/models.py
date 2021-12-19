d854844020f1c5d366d057a2fdbb4c4f84c3116b
alstonlo <40709307+alstonlo@users.noreply.github.com>
2021-12-19 00:34:42 -0500

    adding data augmentation transforms

--- src/akita/models.py
+++ src/akita/models.py
@@ -1,8 +1,58 @@
+import functools
 from argparse import ArgumentParser
 
 import pytorch_lightning as pl
 import torch
 import torch.nn as nn
+
+
+# =============================================================================
+# Data Transforms
+# =============================================================================
+
+
+def shift_pad(seqs, shift, pad=0.25):
+    assert len(seqs.shape) == 3
+    if shift == 0:
+        return seqs
+    seqs = torch.roll(seqs, shifts=shift, dims=1)
+    if shift > 0:
+        seqs[:, :shift, :] = pad
+    else:
+        seqs[:, shift:, :] = pad
+    return seqs
+
+
+def reverse_complement(seqs):
+    assert len(seqs.shape) == 3
+    # flipping dim=1 reverses
+    # flipping dim=2 is a hack to perform complement
+    return torch.flip(seqs, dims=[1, 2])
+
+
+@functools.lru_cache()
+def _reverse_triu_index_perm(width, offset):
+    assert offset > 0
+    triu_idxs = torch.triu_indices(width, width, offset)
+
+    mat = torch.zeros(width, width, dtype=torch.long)
+    mat[triu_idxs[0], triu_idxs[1]] = torch.arange(triu_idxs.shape[1])
+    mat = mat + mat.T
+
+    rev_mat = torch.flip(mat, dims=[0, 1])
+    return rev_mat[triu_idxs[0], triu_idxs[1]]
+
+
+def reverse_triu(trius, width, offset):
+    assert offset > 0
+    assert len(trius.shape) == 3
+    perm = _reverse_triu_index_perm(width, offset)
+    return trius[:, perm, :]
+
+
+# =============================================================================
+# Models
+# =============================================================================
 
 
 class ContactPredictor(nn.Module):
@@ -17,9 +67,9 @@ class ContactPredictor(nn.Module):
         super().__init__()
 
         self.seq_length = seq_length
-        self.seq_depth = self.seq_depth
-        self.target_width = self.target_width
-        self.num_targets = self.num_targets
+        self.seq_depth = seq_depth
+        self.target_width = target_width
+        self.num_targets = num_targets
 
         # TODO: add layers here
 
@@ -38,21 +88,43 @@ class ContactPredictor(nn.Module):
 # Pytorch Lightning wrapper
 class LitContactPredictor(pl.LightningModule):
 
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = ArgumentParser(parents=[parent_parser])
+        parser.add_argument('--augment_shift', type=int, default=11)
+        parser.add_argument('--augment_rc', type=int, default=1)
+        parser.add_argument('--target_crop', type=int, default=32)
+        parser.add_argument('--diagonal_offset', type=int, default=2)
+        parser.add_argument('--lr', type=float, default=1e-4)
+        return parser
+
     def __init__(
             self,
             model: ContactPredictor,
+            augment_shift: int,
+            augment_rc: bool,
+            target_crop: int,
+            diagonal_offset: int,
             lr: float
     ):
         super().__init__()
         self.save_hyperparameters(ignore=["model"])
 
         self.model = model
+        self.augment_shift = augment_shift
+        self.augment_rc = augment_rc
+        self.target_crop = target_crop
+        self.diagonal_offset = diagonal_offset
+        self.lr = lr
+
+        self.out_width = model.target_width - 2 * target_crop
 
     def forward(self, input_seqs):
         return self.model(input_seqs)
 
     def training_step(self, batch, batch_idx):
-        # TODO: compute loss and other metrics
+        batch = self._stochastic_shift_and_rc(batch)
+
         loss = None
         self.log('val_loss', loss)
 
@@ -61,18 +133,11 @@ class LitContactPredictor(pl.LightningModule):
         loss = None
         self.log('val_loss', loss)
 
-    def test_step(self, batch, batch_idx):
-        # TODO: compute loss and other metrics
-        loss = None
-        self.log('test_loss', loss)
-
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
         return optimizer
 
-    @staticmethod
-    def add_model_specific_args(parent_parser):
-        parser = ArgumentParser(parents=[parent_parser], add_help=False)
-        parser.add_argument('--lr', type=float)
-        return parser
-        # TODO: as more params are added to the constructor, update this
+    def _stochastic_shift_and_rc(self, batch):
+        seq, tgt = batch
+
+        return batch
---
+++ tests/test_transforms.py
@@ -0,0 +1,58 @@
+import torch
+
+from src.akita.models import shift_pad, reverse_complement, reverse_triu
+
+
+def test_shift_seq():
+    x = torch.tensor([
+        [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]],
+        [[1, 0, 0, 0], [0, 1, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0]]
+    ]).float()
+
+    shift_x_l = torch.tensor([
+        [[0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1], [0.25, 0.25, 0.25, 0.25]],
+        [[0, 1, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0], [0.25, 0.25, 0.25, 0.25]]
+    ])
+
+    shift_x_r = torch.tensor([
+        [[0.25, 0.25, 0.25, 0.25], [1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]],
+        [[0.25, 0.25, 0.25, 0.25], [1, 0, 0, 0], [0, 1, 0, 0], [1, 0, 0, 0]]
+    ])
+
+    assert torch.equal(shift_x_l, shift_pad(x, shift=-1))
+    assert torch.equal(shift_x_r, shift_pad(x, shift=1))
+
+
+def test_reverse_compliment():
+    x = torch.tensor([
+        [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]],
+        [[1, 0, 0, 0], [0, 1, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0]]
+    ]).float()
+
+    rc_x = torch.tensor([
+        [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]],
+        [[0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]]
+    ]).float()
+
+    assert torch.equal(rc_x, reverse_complement(x))
+
+
+def test_reverse_triu():
+    sym_mat = torch.randn(10, 10)
+    sym_mat = sym_mat + sym_mat.T
+    rev_sym_mat = torch.flip(sym_mat, dims=[0, 1])
+
+    triu_idx = torch.triu_indices(10, 10, offset=2)
+    triu = sym_mat[triu_idx[0], triu_idx[1]]
+    rev_triu = rev_sym_mat[triu_idx[0], triu_idx[1]]
+
+    triu_batch = torch.stack([triu] * 3, dim=0).unsqueeze(2)
+    rev_triu_batch = torch.stack([rev_triu] * 3, dim=0).unsqueeze(2)
+    assert torch.equal(rev_triu_batch, reverse_triu(triu_batch, 10, 2))
+
+
+if __name__ == "__main__":
+    test_shift_seq()
+    test_reverse_compliment()
+    test_reverse_triu()
+    print("Done.")
