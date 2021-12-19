9b5973cf1505557a307a3d9a73d236dfd886a8c7
alstonlo <40709307+alstonlo@users.noreply.github.com>
2021-12-19 03:35:03 -0500

    allowing num_workers > 1 in datamodule

--- src/akita/datamodule.py
+++ src/akita/datamodule.py
@@ -10,14 +10,23 @@ class HDF5SeqDataset(data.Dataset):
 
     def __init__(self, data_path):
         super().__init__()
-        data_file = h5py.File(data_path, "r")
+        self.data_path = data_path
+
+        self.length = len(h5py.File(self.data_path, "r")["sequences"])
+        self.seqs = None
+        self.tgts = None
+
+    def _open_hdf5(self):
+        data_file = h5py.File(self.data_path, "r")
         self.seqs = data_file["sequences"]
         self.tgts = data_file["targets"]
 
     def __len__(self):
-        return self.seqs.len()
+        return self.length
 
     def __getitem__(self, idx):
+        if self.seqs is None:
+            self._open_hdf5()
         seq = np.eye(4)[self.seqs[idx]].astype(np.float32)
         tgt = self.tgts[idx].astype(np.float32)
         return seq, tgt
@@ -25,9 +34,10 @@ class HDF5SeqDataset(data.Dataset):
 
 class AkitaDataModule(pl.LightningDataModule):
 
-    def __init__(self, batch_size=2):
+    def __init__(self, batch_size=2, num_workers=2):
         super().__init__()
         self.batch_size = batch_size
+        self.num_workers = num_workers
 
         akita_dir = pathlib.Path(__file__).parents[2] / "data" / "akita"
         self.train = HDF5SeqDataset(akita_dir / "train.hdf5")
@@ -35,10 +45,25 @@ class AkitaDataModule(pl.LightningDataModule):
         self.test = HDF5SeqDataset(akita_dir / "test.hdf5")
 
     def train_dataloader(self):
-        return data.DataLoader(self.train, batch_size=self.batch_size, shuffle=True)
+        return data.DataLoader(
+            self.train,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers
+        )
 
     def val_dataloader(self):
-        return data.DataLoader(self.valid, batch_size=self.batch_size, shuffle=False)
+        return data.DataLoader(
+            self.valid,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers
+        )
 
     def test_dataloader(self):
-        return data.DataLoader(self.valid, batch_size=self.batch_size, shuffle=False)
+        return data.DataLoader(
+            self.test,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers
+        )
