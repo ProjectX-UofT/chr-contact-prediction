0b0cec22526dd861283ebb1c2c747022739dae0f
alstonlo <40709307+alstonlo@users.noreply.github.com>
2022-02-01 18:46:14 -0500

    improve efficiency

--- src/akita/metrics.py
+++ src/akita/metrics.py
@@ -1,6 +1,7 @@
 import pathlib
 import statistics
 
+import torch
 import torch.nn.functional as F
 from scipy import stats
 from tqdm import tqdm
@@ -9,53 +10,35 @@ from src.akita.datamodule import AkitaDataModule
 from src.akita.models import LitContactPredictor
 
 
-def calculate_mse(model, batch, target_idx):
-    seqs, tgts = batch
-    preds = model(seqs)
-
-    preds = preds[0, :, target_idx]
-    tgts = tgts[0, :, target_idx]
-    return F.mse_loss(preds, tgts).item()
-
-
-def calculate_pearson_r(model, batch, target_idx):
-    seqs, tgts = batch
-    preds = model(seqs)
-
-    preds = preds[0, :, target_idx]
-    tgts = tgts[0, :, target_idx]
-    return stats.pearsonr(preds, tgts)[0]
-
-
-def calculate_spearman_r(model, batch, target_idx):
-    seqs, tgts = batch
-    preds = model(seqs)
-
-    preds = preds[0, :, target_idx]
-    tgts = tgts[0, :, target_idx]
-    return stats.spearmanr(preds, tgts)[0]
-
-
 def metrics_main():
     ckpt_path = str(pathlib.Path(__file__).parents[2] / "checkpoints" / "model-1mx0k8pg.ckpt")
     model = LitContactPredictor.load_from_checkpoint(ckpt_path)
     datamodule = AkitaDataModule(batch_size=1, num_workers=4)
 
+    mses = [list() for _ in range(5)]
+    spearmans = [list() for _ in range(5)]
+    pearsons = [list() for _ in range(5)]
+    for batch in tqdm(datamodule.test_dataloader(), leave=False, total=32):
+
+        with torch.no_grad():
+            with model.ema.average_parameters():
+                seqs, tgts = batch
+                preds = model(seqs)
+
+        for idx in range(5):
+            y_pred = preds[:, :, idx].detach()
+            y_true = tgts[:, :, idx].detach()
+            mses[idx].append(F.mse_loss(y_pred, y_true).item())
+
+            y_pred, y_true = y_pred[0].numpy(), y_true[0].numpy()
+            spearmans[idx].append(stats.spearmanr(y_pred, y_true)[0])
+            pearsons[idx].append(stats.pearsonr(y_pred, y_true)[0])
+
     for idx in range(5):
         print("Dataset", idx)
-
-        mses = []
-        spearmans = []
-        pearsons = []
-        for batch in tqdm(datamodule.test_dataloader(), leave=False, total=32):
-            with model.ema.average_parameters():
-                mses.append(calculate_mse(model, batch, idx))
-                spearmans.append(calculate_spearman_r(model, batch, idx))
-                pearsons.append(calculate_pearson_r(model, batch, idx))
-
-        print("\tMSE", statistics.mean(mses))
-        print("\tSpearman", statistics.mean(spearmans))
-        print("\tPearson", statistics.mean(pearsons))
+        print("\tMSE", statistics.mean(mses[idx]))
+        print("\tSpearman", statistics.mean(spearmans[idx]))
+        print("\tPearson", statistics.mean(pearsons[idx]))
 
 
 if __name__ == '__main__':
